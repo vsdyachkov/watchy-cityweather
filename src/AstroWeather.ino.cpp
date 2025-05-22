1ebab72fc4ec9745b0aee11dd0d4586d60be7591
Viktor <vsdyachkov@rambler.ru>
2025-05-22 22:08:50 +0300

    part1

--- src/AstroWeather.ino.cpp
+++ src/AstroWeather.ino.cpp
@@ -1,12 +1,58 @@
 #include "CityWeather.h"
 #include "settings.h"
+#include "esp32notifications.h"
 
 CityWeather watchy(settings);
+
+BLENotifications notifications;
+uint32_t incomingCallNotificationUUID;
+
+void onBLEStateChanged(BLENotifications::State state) {
+  switch(state) {
+      case BLENotifications::StateConnected:
+          Serial.println("StateConnected - connected to a phone or tablet"); 
+          break;
+
+      case BLENotifications::StateDisconnected:
+          Serial.println("StateDisconnected - disconnected from a phone or tablet"); 
+          notifications.startAdvertising(); 
+          break; 
+  }
+}
+
+void onNotificationArrived(const ArduinoNotification * notification, const Notification * rawNotificationData) {
+    Serial.print("Got notification: ");   
+    Serial.println(notification->title); // The title, ie name of who sent the message
+    Serial.println(notification->message); // The detail, ie "be home for dinner at 7".
+    Serial.println(notification->type);  // Which app sent it
+    Serial.println(notifications.getNotificationCategoryDescription(notification->category));  // ie "social media"
+    Serial.println(notification->categoryCount); // How may other notifications are there from this app (ie badge number)
+    if (notification->category == CategoryIDIncomingCall) {
+		// If this is an incoming call, store it so that we can later send a user action.
+        incomingCallNotificationUUID = notification->uuid;
+        Serial.println("--- INCOMING CALL: PRESS A TO ACCEPT, C TO REJECT ---"); 
+    }
+    else {
+        incomingCallNotificationUUID = 0; // Make invalid - no incoming call
+    }
+}
+
+void onNotificationRemoved(const ArduinoNotification * notification, const Notification * rawNotificationData) {
+     Serial.print("Removed notification: ");   
+     Serial.println(notification->title);
+     Serial.println(notification->message);
+     Serial.println(notification->type);  
+}
 
 void setup(){
   Serial.begin(115200);
   while (!Serial);
   watchy.init();
+
+  notifications.begin("Watchy");
+  notifications.setConnectionStateChangedCallback(onBLEStateChanged);
+  notifications.setNotificationCallback(onNotificationArrived);
+  notifications.setRemovedCallback(onNotificationRemoved);
 }
 
 void loop(){}
